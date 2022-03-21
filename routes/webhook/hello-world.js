aa9a36293e61195b18c8c8a4560b060b53000291
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-21 00:46:52 -0700

    Add package json and console .log to string

--- routes/webhook/hello-world.js
+++ routes/webhook/hello-world.js
@@ -4,6 +4,8 @@ const router = Router();
 // /webhook/hello-world
 router.post("/", (req, res) => {
   console.log(req.body);
+	console.log(`-------------------------------`);
+	console.log(JSON.stringify(req.body));
   res.sendStatus(200);
 });
 
