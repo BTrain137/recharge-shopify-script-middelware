4a1007da62084b2e7cd72191ea1ea0034bc7dd75
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-21 00:58:54 -0700

    Initial order create

--- routes/webhook/index.js
+++ routes/webhook/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const helloWorld = require("./hello-world.js");
+const recharge = require("./recharge.js");
 
 const router = Router();
 
 // Test Route
 // /webhook/
 router.use("/hello-world", helloWorld);
+router.use("/recharge", recharge);
 
 module.exports = router;
---
+++ routes/webhook/recharge.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const router = Router();
+
+// /webhook/hello-world
+router.post("/order-created", (req, res) => {
+  console.log(req.body);
+	console.log(`-------------------------------`);
+	console.log(JSON.stringify(req.body));
+  res.sendStatus(200);
+});
+
+module.exports = router;
